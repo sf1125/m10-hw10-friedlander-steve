18739f358de443092bd589d3c441c648e9125d49
Steve Friedlander <s.friedlander@ufl.edu>
2021-07-12 18:57:13 -0400

    Added code for homework and babel files

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const Boiling = getDocumentById(boiling-temp) => console.log(boiling-temp);
+
+boiling("212");
+
+const Freezing = getDocumentById(freezing-temp) => console.log(freezing-temp);
+
+freezing("32");
+
+const Water = getDocumentById(water-temp) => console.log(water-temp);
+
+water("72");
